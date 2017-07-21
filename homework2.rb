50c4f0dd79a49e6a1fc4323327ee1739d1fe3457
jeff <jeff@ulink.com.tw>
2017-07-21 14:44:08 +0800

    add won_times

--- homework2.rb
+++ homework2.rb
@@ -2,6 +2,7 @@
 class Player
     def initialize(name)
         @name = name
+        
     end
 end
 
@@ -24,6 +25,7 @@ class RPS
         name = gets.chomp
         puts "hello #{name}" 
         play
+        pay
         puts "Bye! Bye! #{name}"
     end
 
@@ -44,18 +46,22 @@ class RPS
     end
 
     def who_won
+        $won_times = 0
       if @user_input == "R"
          if @computer_input == "R" && @user_input == "R"
               puts "the game is drawn!"
             elsif @computer_input == "P" && @user_input == "R"
                 puts "you lose!"
          else @computer_input == "S" && @user_input == "R"
-              puts "you won!"
+              puts "you won! "
+              $won_times +=1
+              
           end
         end
       if @user_input == "P"
         if @computer_input == "R" && @user_input == "P"
               puts "you won!"
+              $won_times +=1
             elsif @computer_input == "P" && @user_input == "P"
                 puts "the game is drawn!"
             else @computer_input == "S" && @user_input == "P"
@@ -67,6 +73,7 @@ class RPS
               puts "you lose!"
             elsif @computer_input == "P" && @user_input == "S"
                   puts "you won!"
+                  $won_times +=1
         else @computer_input == "S" && @user_input == "S"
                 puts "the game is drawn!!"
         end
@@ -83,6 +90,12 @@ class RPS
             when "N"
                 flag = false
         end
+    end
+
+#the private code not yet reday!
+    private
+    def pay
+        puts "you won #{$won_times}"
     end
        
 end
